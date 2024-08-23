bfa1f532b1cba1cefaf7594d5b52c998602d3fd0
Shivang4020 <107241759+Shivang4020@users.noreply.github.com>
2024-08-24 02:02:04 +0530

    step 1

---
+++ propartyowl/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './nav/Nav'
+import Hero from './hero/Hero'
+
+const Home = () => {
+  return (
+    <>
+    <Nav />
+    <Hero />
+    </>
+  )
+}
+
+export default Home
---
+++ propartyowl/src/components/home/nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className=' w-full py-1 px-2 bg-black text-white ' >
+        <div>
+            <img src="" alt="" />
+        </div>
+        <ul className=' flex ' >
+            <li>Home</li>
+            <li>Propertys</li>
+            <li>Agents</li>
+            <li>Login</li>
+            <li>Register</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Nav
